c477fa4bf88becd1bc32ec56b0b904fa1c735219
Rais Ilham <raispinter12@gmail.com>
2023-01-18 20:08:53 +0700

    Dark mode toogle

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-
+  darkMode: 'class',
   content: [
     './src/**/*.{html,js}',
     './dist/**/*.{html,js}',
     './index.html',
+    './node_modules/flowbite/**/*.js'
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ]
 }
